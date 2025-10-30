ee5394b46306d2f70b43aa98a6f46185560f75f6
thaichors3000 <thaichorseng@gmail.com>
2025-10-30 20:38:21 +0700

    add lexxy

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+
+pin "lexxy", to: "lexxy.js"
+pin "@rails/activestorage", to: "activestorage.esm.js" # to support attachments
